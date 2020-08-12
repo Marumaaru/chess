442fb6079ba71068c615826a60ade4e289ea6215
pandenok <dimitri.shatunin@gmail.com>
2020-08-12 13:04:07 +0200

    Initial commit

---
+++ lib/board.rb
@@ -0,0 +1,19 @@
+class Board
+  attr_reader :board
+  
+  SIZE = 8
+
+  def initialize
+    @board = Array.new(SIZE) { Array.new(SIZE, ' ') }
+  end
+
+  def show
+    puts "\n    a   b   c   d   e   f   g   h  "
+    puts '  +---+---+---+---+---+---+---+---+'
+    board.each_with_index do |row, idx|
+      puts "#{board.size - idx} | " + row.each { |square| square }.join(' | ') + " | #{board.size - idx}"
+      puts '  +---+---+---+---+---+---+---+---+'
+    end
+    puts '    a   b   c   d   e   f   g   h  '
+  end
+end
