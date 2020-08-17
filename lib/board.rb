4c88bc2ea86f1f331321d513a1d191adf67c1a29
pandenok <dimitri.shatunin@gmail.com>
2020-08-17 22:41:08 +0200

    Add Board#valid_move? and tests for all pieces

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,133 @@
+require 'pry'
 class Board
   attr_reader :board
   
   SIZE = 8
+  EMPTY_SQUARE = ' '
 
   def initialize
-    @board = Array.new(SIZE) { Array.new(SIZE, ' ') }
+    @board = Array.new(SIZE) { Array.new(SIZE, EMPTY_SQUARE) }
+  end
+
+  def valid_move?(src, trg)
+    if src.class == Pawn
+      if src.rank == 6 || src.rank == 1
+        if (src.rank - trg.rank).abs <= 2
+          true
+        else
+          puts "Invalid move"
+          false
+        end
+      elsif src.color == 'white'
+        if (src.rank - trg.rank) > 0
+          true
+        else
+          puts "Invalid move"
+          false
+        end
+      elsif src.color == 'black'
+        if (src.rank - trg.rank) < 0
+          true
+        else
+          puts "Invalid move"
+          false
+        end
+      else
+        if (src.rank - trg.rank).abs == 1 && src.file == trg.file
+          true
+        else
+          puts "Invalid move"
+          false
+        end
+      end
+    elsif src.class == Knight
+      if ((src.file - trg.file).abs == 1 && (src.rank - trg.rank).abs == 2) ||
+        ((src.file - trg.file).abs == 2 && (src.rank - trg.rank).abs == 1)
+        true
+      else
+        puts 'Invalid move'
+        false
+      end
+    elsif src.class == Bishop
+      if (src.rank - trg.rank).abs == (src.file - trg.file).abs
+        true
+      else
+        puts 'Invalid move'
+        false
+      end
+    elsif src.class == Rook
+      if src.rank == trg.rank || src.file == trg.file
+        true
+      else
+        puts 'Invalid move'
+        false
+      end
+    elsif src.class == King
+      if (src.rank - trg.rank).abs <= 1 && (src.file - trg.file).abs <= 1
+        true
+      else
+        puts 'Invalid move'
+        false
+      end
+    elsif src.class == Queen
+      if (src.rank - trg.rank).abs == (src.file - trg.file).abs ||
+          src.rank == trg.rank || src.file == trg.file
+        true
+      else
+        puts 'Invalid move'
+        false
+      end
+    else
+      true
+    end
+  end
+
+  # def origin_square(from)
+  #   board[from[1]][from[0]]
+  # end
+
+  # def target_square(from, to)
+  #   from.class.new(to[0], to[1])
+  # end
+
+  def path_free?(route)
+    route[1..route.size].all? { |coords| board[coords[1]][coords[0]] == EMPTY_SQUARE }
+  end
+
+  def bfs_traversal(src, trg, queue = [])
+    queue << src
+    until queue.empty?
+      current = queue.shift
+      return current if current.file == trg.file && current.rank == trg.rank
+
+      current.where_can_jump_from_here.each { |child| queue << child }
+    end
+  end
+
+  def piece_moves(from, to)
+    src = board[from[1]][from[0]]
+    trg = src.class.new(to[0], to[1], src.color)
+    # bfs_traversal(src, trg)
+    traversal = bfs_traversal(src, trg)
+    route = route(traversal)
+    if path_free?(route) && valid_move?(src, trg)
+      place(trg)
+      clean(src)
+      show
+    end
+    # route.each { |move| board[move[1]][move[0]] = '*' }
+  end
+
+  #Queen and Pawn non working moves!
+
+
+  def route(node, route = [])
+    if node.parent.nil?
+      route << [node.file, node.rank]
+      return route
+    end
+    route(node.parent, route)
+    route << [node.file, node.rank]
   end
 
   def show
@@ -20,10 +143,13 @@ class Board
   def mapped_board
     board.map do |row|
       row.map do |square| 
-        if square == ' '
-          " "
+        if square == EMPTY_SQUARE
+          EMPTY_SQUARE
+        elsif square == '*'
+          '*'
         else
-          square.symbol
+          # square.symbol
+          square.name
         end
       end
     end
@@ -34,7 +160,7 @@ class Board
   end
 
   def clean(piece)
-    board[piece.rank][piece.file] = ' '
+    board[piece.rank][piece.file] = EMPTY_SQUARE
   end
 
   def find_pieces_by(input)
@@ -42,7 +168,7 @@ class Board
     list_of_pieces = []
     board.each do |row|
       row.each do |square|
-        list_of_pieces << square if square.name.eql?(first_letter) unless square == " "
+        list_of_pieces << square if square.name.eql?(first_letter) unless square == EMPTY_SQUARE
       end
     end
     list_of_pieces
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,10 @@
 require './lib/board'
 require './lib/knight'
+require './lib/bishop'
+require './lib/rook'
+require './lib/pawn'
+require './lib/king'
+require './lib/queen'
 
 describe Board do
   subject(:test) { described_class.new }
@@ -64,7 +69,7 @@ describe Board do
         test.place(knight4)
       end
 
-      it 'outputs a pretty formatted grid with pieces symbols' do
+      xit 'outputs a pretty formatted grid with pieces symbols' do
         printed_board = 
           <<~HEREDOC
 
@@ -85,6 +90,33 @@ describe Board do
           2 |   |   |   |   |   |   |   |   | 2
             +---+---+---+---+---+---+---+---+
           1 |   | ♘ |   |   |   |   | ♘ |   | 1
+            +---+---+---+---+---+---+---+---+
+              a   b   c   d   e   f   g   h  
+          HEREDOC
+        expect { test.show }.to output((printed_board)).to_stdout
+      end
+
+      it 'outputs a pretty formatted grid with piece initials' do
+        printed_board = 
+          <<~HEREDOC
+
+              a   b   c   d   e   f   g   h  
+            +---+---+---+---+---+---+---+---+
+          8 |   | N |   |   |   |   | N |   | 8
+            +---+---+---+---+---+---+---+---+
+          7 |   |   |   |   |   |   |   |   | 7
+            +---+---+---+---+---+---+---+---+
+          6 |   |   |   |   |   |   |   |   | 6
+            +---+---+---+---+---+---+---+---+
+          5 |   |   |   |   |   |   |   |   | 5
+            +---+---+---+---+---+---+---+---+
+          4 |   |   |   |   |   |   |   |   | 4
+            +---+---+---+---+---+---+---+---+
+          3 |   |   |   |   |   |   |   |   | 3
+            +---+---+---+---+---+---+---+---+
+          2 |   |   |   |   |   |   |   |   | 2
+            +---+---+---+---+---+---+---+---+
+          1 |   | N |   |   |   |   | N |   | 1
             +---+---+---+---+---+---+---+---+
               a   b   c   d   e   f   g   h  
           HEREDOC
@@ -158,6 +190,330 @@ describe Board do
         list_of_knights = [knight3, knight4, knight1, knight2]
         result = test.find_pieces_by(input)
         expect(result).to eq(list_of_knights)
+      end
+    end
+  end
+
+  describe "#piece_moves(from, to)" do
+    let(:bishop) { Bishop.new(2, 7) }
+
+    before do
+      test.place(bishop)
+    end
+
+    context 'when moving a bishop' do
+      xit 'trg square class is Bishop' do
+        from = [2, 7]
+        to = [6, 3]
+        result = test.piece_moves(from, to).class
+        expect(result).to eq(Bishop)
+      end
+    end
+  end
+
+  describe "#route(node)" do
+    let(:bishop) { Bishop.new(2, 7) }
+    let(:rook) { Rook.new(0, 7) }
+
+    before do
+      test.place(bishop)
+      test.place(rook)
+    end
+
+    context 'when moving a bishop' do
+      xit 'shows the legal path of bishop on the board' do
+        from = [2, 7]
+        to = [6, 3]
+        path = [[2, 7], [3, 6], [4, 5], [5, 4], [6, 3]]
+        traversal = test.piece_moves(from, to)
+        route = test.route(traversal)
+        expect(route).to eq(path)
+      end
+    end
+
+    context 'when moving a rook' do
+      xit 'shows the legal path of rook on the board' do
+        from = [0, 7]
+        to = [0, 1]
+        path = [[0, 7], [0, 6], [0, 5], [0, 4], [0, 3], [0, 2], [0, 1]]
+        traversal = test.piece_moves(from, to)
+        route = test.route(traversal)
+        expect(route).to eq(path)
+      end
+    end
+  end
+
+  describe "#path_free?(from, to)" do
+    let(:bishop) { Bishop.new(2, 7, 'white') }
+    let(:knight) { Knight.new(3, 6, 'white') }
+
+    before do 
+      test.place(bishop)
+    end
+
+    context 'when there are no other pieces on the path' do
+      it 'is free' do
+        to = [6, 3]
+        src = bishop
+        trg = src.class.new(to[0], to[1], src.color)
+        traversal = test.bfs_traversal(src, trg)
+        route = test.route(traversal)
+        result = test.path_free?(route)
+        expect(result).to eq(true)
+      end
+    end
+
+    context 'when the path is obstructed' do
+      it 'is not free' do
+        test.place(knight)
+        to = [6, 3]
+        src = bishop
+        trg = src.class.new(to[0], to[1], src.color)
+        traversal = test.bfs_traversal(src, trg)
+        route = test.route(traversal)
+        result = test.path_free?(route)
+        expect(result).to eq(false)
+      end
+    end
+  end
+
+  describe "#valid_move?(src, trg)" do
+    context 'when a Pawn moves' do
+      context 'when a Pawn makes its first move' do
+        let(:src) { Pawn.new(1, 6, 'white') }
+        let(:trg) { Pawn.new(1, 4, 'white') }
+        it 'can jump over 1 square' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a Pawn moves 1 square forward' do
+        let(:src) { Pawn.new(1, 5, 'white') }
+        let(:trg) { Pawn.new(1, 4, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a Pawn moves 1 square backward' do
+        let(:src) { Pawn.new(1, 4, 'white') }
+        let(:trg) { Pawn.new(1, 5, 'white') }
+        it 'is not valid' do
+          allow(test).to receive(:puts)
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(false)
+        end
+      end
+
+      context 'when a Pawn moves 1 square diagonally' do
+        let(:src) { Pawn.new(1, 5, 'white') }
+        let(:trg) { Pawn.new(2, 5, 'white') }
+        it 'is not valid' do
+          allow(test).to receive(:puts)
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(false)
+        end
+      end
+
+      context 'when a Pawn moves 1 square horizontally' do
+        let(:src) { Pawn.new(1, 5, 'white') }
+        let(:trg) { Pawn.new(2, 5, 'white') }
+        it 'is not valid' do
+          allow(test).to receive(:puts)
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(false)
+        end
+      end
+
+      context 'when a Knight makes a legal move' do
+        let(:src) { Knight.new(1, 7, 'white') }
+        let(:trg) { Knight.new(0, 5, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+    end
+
+    context 'when a Knight moves' do
+      context 'when a Knight makes a legal move' do
+        let(:src) { Knight.new(1, 7, 'white') }
+        let(:trg) { Knight.new(0, 5, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a Knight makes a non legal move' do
+        let(:src) { Knight.new(1, 7, 'white') }
+        let(:trg) { Knight.new(1, 3, 'white') }
+        it 'is not valid' do
+          allow(test).to receive(:puts)
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(false)
+        end
+      end
+    end
+
+    context 'when a Bishop moves' do
+      context 'when a Bishop makes a legal move' do
+        let(:src) { Bishop.new(2, 7, 'white') }
+        let(:trg) { Bishop.new(6, 3, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a Bishop makes a non legal move' do
+        let(:src) { Bishop.new(2, 7, 'white') }
+        let(:trg) { Bishop.new(3, 3, 'white') }
+        it 'is not valid' do
+          allow(test).to receive(:puts)
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(false)
+        end
+      end
+    end
+
+    context 'when a Rook moves' do
+      context 'when a Rook makes a legal vertical move' do
+        let(:src) { Rook.new(0, 7, 'white') }
+        let(:trg) { Rook.new(0, 4, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a Rook makes a non legal vertical move' do
+        let(:src) { Rook.new(0, 7, 'white') }
+        let(:trg) { Rook.new(1, 6, 'white') }
+        it 'is not valid' do
+          allow(test).to receive(:puts)
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(false)
+        end
+      end
+
+      context 'when a Rook makes a legal horizontal move' do
+        let(:src) { Rook.new(0, 4, 'white') }
+        let(:trg) { Rook.new(7, 4, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a Rook makes a non legal horizontal move' do
+        let(:src) { Rook.new(0, 4, 'white') }
+        let(:trg) { Rook.new(4, 5, 'white') }
+        it 'is not valid' do
+          allow(test).to receive(:puts)
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(false)
+        end
+      end
+    end
+
+    context 'when a King moves' do
+      context 'when a King moves 1 square vertically' do
+        let(:src) { King.new(4, 4, 'white') }
+        let(:trg) { King.new(4, 5, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a King moves 1 square horizontally' do
+        let(:src) { King.new(4, 4, 'white') }
+        let(:trg) { King.new(3, 4, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a King moves 1 square diagonally' do
+        let(:src) { King.new(4, 4, 'white') }
+        let(:trg) { King.new(5, 5, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a King makes a non legal move' do
+        let(:src) { King.new(4, 4, 'white') }
+        let(:trg) { King.new(4, 6, 'white') }
+        it 'is not valid' do
+          allow(test).to receive(:puts)
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(false)
+        end
+      end
+    end
+
+    context 'when a Queen moves' do
+      context 'when a Queen make a legal diagonal move' do
+        let(:src) { Queen.new(3, 4, 'white') }
+        let(:trg) { Queen.new(0, 7, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a Queen makes a non legal diagonal move' do
+        let(:src) { Queen.new(3, 4, 'white') }
+        let(:trg) { Queen.new(0, 6, 'white') }
+        it 'is not valid' do
+          allow(test).to receive(:puts)
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(false)
+        end
+      end
+
+      context 'when a Queen makes a legal vertical move' do
+        let(:src) { Queen.new(3, 4, 'white') }
+        let(:trg) { Queen.new(3, 0, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a Queen makes a non vertical legal move' do
+        let(:src) { Queen.new(3, 4, 'white') }
+        let(:trg) { Queen.new(2, 7, 'white') }
+        it 'is not valid' do
+          allow(test).to receive(:puts)
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(false)
+        end
+      end
+
+      context 'when a Queen makes a legal horizontal move' do
+        let(:src) { Queen.new(3, 4, 'white') }
+        let(:trg) { Queen.new(7, 4, 'white') }
+        it 'is valid' do
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when a Queen makes a non legal horizontal move' do
+        let(:src) { Queen.new(3, 4, 'white') }
+        let(:trg) { Queen.new(7, 3, 'white') }
+        it 'is not valid' do
+          allow(test).to receive(:puts)
+          result = test.valid_move?(src, trg)
+          expect(result).to eq(false)
+        end
       end
     end
   end
